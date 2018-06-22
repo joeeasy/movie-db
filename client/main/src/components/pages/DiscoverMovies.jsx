4d005373885e8c818ef3bb1e98d99d0e1a3b646a
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-06-22 23:53:45 +0100

    chore(ui): setup UI branch
    - clear unneccesary data in ui branch
    
    - setup UI templates to be work with github pages
    
    [fixes #UITemplate]

--- client/main/src/components/pages/DiscoverMovies.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import DiscoverItems from "../common/DiscoverItems";
-
-class DiscoverMovies extends Component {
-  render() {
-    return <DiscoverItems />;
-  }
-}
-export default DiscoverMovies;
